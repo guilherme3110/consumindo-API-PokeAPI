dc90b0d4e22bbba7e18fb307d4dda7fd52a6d87d
guilherme3110 <guilherme311094@hotmail.com>
2022-12-30 14:47:57 -0300

    pokedex

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,29 @@
-/* Monte sua lógica aqui */
+function render(array) {
+    const mainList = document.querySelector('.main__list')
+
+    mainList.innerHTML = ''
+
+    array.forEach(element => {
+        const card = createCard(element)
+        
+        mainList.append(card)
+    })
+}
+
+function createCard({name, img, ability}) {
+    const container = document.createElement('li')
+    const image = document.createElement('img')
+    const h2 = document.createElement('h2')
+    const p = document.createElement('p')
+
+    image.src = img
+    image.alt = name
+
+    h2.innerText = name
+
+    p.innerText = ability
+
+    container.append(image, h2, p)
+
+    return container
+}
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,18 @@
-/* Monte a lógica das requisições aqui */
+async function getAllPokemons() {
+  const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  .then(res => res.json())
+  .then(res => {
+   
+    console.log(res)
+  })
+
+  
+}
+
+getAllPokemons()
