217af4ab28b16a93b6aba0bc1d788f01256387cc
Guilherme Scaramuzzi <guilherme311094@hotmail.com>
2022-12-31 12:41:32 -0300

    pokedex

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,28 +2,34 @@ function render(array) {
     const mainList = document.querySelector('.main__list')
 
     mainList.innerHTML = ''
-
+  
     array.forEach(element => {
-        const card = createCard(element)
-        
-        mainList.append(card)
-    })
-}
 
-function createCard({name, img, ability}) {
+      console.log(element)
+      const img = element.url.slice(34, -1)
+      console.log(img)
+      const card = createCard(element, img)
+
+  
+      mainList.append(card)
+    })
+  }
+  
+  function createCard({name}, img) {
     const container = document.createElement('li')
     const image = document.createElement('img')
     const h2 = document.createElement('h2')
-    const p = document.createElement('p')
-
-    image.src = img
+   
+    image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${img}.png`
     image.alt = name
-
     h2.innerText = name
-
-    p.innerText = ability
-
-    container.append(image, h2, p)
-
+  
+   
+  
+    container.append(image, h2, )
+  
     return container
-}
+  }
+  
+
+  
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,5 +1,25 @@
 async function getAllPokemons() {
-  const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon', {
+
+  const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon`, {
+    method:'GET',
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(res => {
+    
+    render(res.results)
+    //console.log(res)
+
+    return res
+  })
+  return pokemons
+}
+
+async function getPokemonByName(pokemonName) {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}
+  `, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
@@ -8,11 +28,23 @@ async function getAllPokemons() {
 
   .then(res => res.json())
   .then(res => {
-   
-    console.log(res)
+    render(res)
+    return res
   })
-
-  
+  return pokemon
 }
 
+function renderSeach(){
+   const searchInput = document.querySelector('input')
+   const seachBtn = document.querySelector('#searchBtn')
+  
+    seachBtn.addEventListener('click', () => {
+
+      getPokemonByName(searchInput.value)
+    })
+}
+
+
 getAllPokemons()
+//getPokemonByName()
+renderSeach()
